0de901194e4fe6cd701f2eda2ce6fa7a3d98360b
Benjamin <benjaminayimah@gmail.com>
2023-05-20 02:34:28 +0000

    chat

--- src/components/includes/app/MessageLists.vue
+++ src/components/includes/app/MessageLists.vue
@@ -1,8 +1,8 @@
 <template>
     <li>
-        <router-link :to="{ name: 'MessageDetail', params: { id: message.id, name: message.name }}" href="" class="flx gap-8 bg-white br-16 row-hover relative" :class="{ 'new-chat' : message.unread_count > 0}">
+        <router-link :to="{ name: 'MessageDetail', params: { id: message.id, name: message.name }}" href="" class="flx gap-8 bg-white br-16 relative" :class="{ 'new-chat' : message.unread_count > 0}">
             <profile-avatar :id="null" :image="null" />
-            <div class="flx column gap-8 ms">
+            <div class="flx column gap-8 ms flx-grow-1">
                 <div class="flx jc-sb">
                     <label class="wrap-text wrap-line-1">{{ message.name }}</label>
                     <span class="gray fs-08">{{ message.updated_at }}</span>
@@ -42,14 +42,17 @@ a {
     padding: 10px;
     border: 1px solid transparent;
     transition: var(--input-btn-transition);
+    &:hover {
+        background-color: rgba(224, 225, 237, 0.4);
+    }
 }
 .new-chat {
+    background-color: rgba(1, 115, 255, 0.1) !important;
     label, .highlight {
         color: var(--ft-dark);
     }
 }
 .router-link-exact-active{
-    background-color: rgba(1, 115, 255, 0.1);
     border-color: var(--primary-color);
 }
 
