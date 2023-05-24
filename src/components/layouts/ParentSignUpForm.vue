a48072e40e510882ed6180a86f7b353bd841f6a4
Benjamin <benjaminayimah@gmail.com>
2023-05-24 19:32:46 +0000

    chat and dashboard

--- src/components/layouts/ParentSignUpForm.vue
+++ src/components/layouts/ParentSignUpForm.vue
@@ -109,6 +109,10 @@ export default {
             this.validation.error = true
             this.validation.errors = e.response.data.errors
           }
+          if (e.response.status == 503) {
+            this.userError.error = true
+            this.userError.message = 'Our system is currently down for upgrade. Please try again later. Sorry for the inconvenience.'
+          }
         })
       },
       signupSuccess(res) {
