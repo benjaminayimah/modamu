e92f7a0a3b83923ce3531cb99e59b63cc2874332
Benjamin <benjaminayimah@gmail.com>
2023-06-11 17:18:39 +0000

    seach update

--- src/views/app/Message.vue
+++ src/views/app/Message.vue
@@ -13,7 +13,7 @@
                                 <path d="M-3384.438-704.614l-1.494-1.5c-1.017.746-2.525,1.083-4.788,1.083-2.569,0-4.164-.435-5.174-1.408-1.052-1.015-1.521-2.646-1.521-5.288s.469-4.273,1.521-5.288c1.01-.974,2.605-1.408,5.174-1.408s4.164.434,5.174,1.408c1.053,1.014,1.522,2.645,1.522,5.288,0,2.244-.338,3.759-1.085,4.786l1.5,1.5a.583.583,0,0,1,0,.824.58.58,0,0,1-.412.171A.582.582,0,0,1-3384.438-704.614Zm-10.647-11.556c-.795.766-1.165,2.18-1.165,4.448s.37,3.682,1.165,4.449c.775.747,2.121,1.08,4.364,1.08s3.589-.333,4.364-1.08c.795-.767,1.165-2.181,1.165-4.449s-.37-3.682-1.165-4.448c-.775-.748-2.12-1.081-4.364-1.081S-3394.311-716.918-3395.085-716.17Z" transform="translate(3397.417 718.417)" fill="#212135"/>
                             </svg>
                         </i>
-                        <input class="form-control" type="search" placeholder="Search message...">
+                        <input class="form-control" v-model="search" type="search" ref="searchInput" placeholder="Search message...">
                     </div>
                     <button @click="toggleSearch" class="toggle-btn br-50">
                         <svg v-if="searchToggle" xmlns="http://www.w3.org/2000/svg" height="14.683" viewBox="0 0 14.647 14.683">
@@ -28,9 +28,10 @@
             <div v-if="!messages.length" class="bg-white mt-24 pd-24 br-16 centered">
                 No Messages
             </div>
-            <div v-else class="flx column gap-16 message-body">
-                <message-lists v-for="data in messages" :key="data.message.id" :data="data"/>
+            <div v-else-if="computedItem.length" class="flx column gap-16 message-body">
+                <message-lists v-for="data in computedItem" :key="data.message.id" :data="data"/>
             </div>
+            <div v-else class="mt-16"><strong>No search result for:</strong> <i>{{ search }}</i></div>
         </div>
         <div class="message-right h-100 flx-grow-1">
             <router-view></router-view>
@@ -46,16 +47,30 @@ export default {
     computed: {
         ...mapState({
             messages: (state) => state.messages
-        })
+        }),
+        computedItem() {
+            if(this.search !=='')
+            return this.messages.filter(item => {
+                return item.sender.name.toLowerCase().match(this.search.toLowerCase()) || item.message.preview.toLowerCase().match(this.search.toLowerCase())
+            })
+            else
+            return this.messages
+        }
     },
     data () {
         return {
-            searchToggle: false
+            searchToggle: false,
+            search: ''
         }
     },
     methods: {
         toggleSearch() {
             this.searchToggle = !this.searchToggle
+            this.$nextTick(function () {
+                if(this.searchToggle) {
+                    this.$refs.searchInput.focus()
+                }
+            })
         }
     },
     mounted () {
--- src/views/app/admin/Parents.vue
+++ src/views/app/admin/Parents.vue
@@ -11,22 +11,23 @@
                         <path d="M-3384.438-704.614l-1.494-1.5c-1.017.746-2.525,1.083-4.788,1.083-2.569,0-4.164-.435-5.174-1.408-1.052-1.015-1.521-2.646-1.521-5.288s.469-4.273,1.521-5.288c1.01-.974,2.605-1.408,5.174-1.408s4.164.434,5.174,1.408c1.053,1.014,1.522,2.645,1.522,5.288,0,2.244-.338,3.759-1.085,4.786l1.5,1.5a.583.583,0,0,1,0,.824.58.58,0,0,1-.412.171A.582.582,0,0,1-3384.438-704.614Zm-10.647-11.556c-.795.766-1.165,2.18-1.165,4.448s.37,3.682,1.165,4.449c.775.747,2.121,1.08,4.364,1.08s3.589-.333,4.364-1.08c.795-.767,1.165-2.181,1.165-4.449s-.37-3.682-1.165-4.448c-.775-.748-2.12-1.081-4.364-1.081S-3394.311-716.918-3395.085-716.17Z" transform="translate(3397.417 718.417)" fill="#212135"/>
                     </svg>
                 </i>
-                <input class="form-control" type="search" data-type="icon" placeholder="Search parents...">
+                <input class="form-control" v-model="search" type="search" data-type="icon" placeholder="Search parents...">
             </div>
         </div>
         <div id="grid_table">
-            <div class="grid table-body">
+            <div v-if="!parents.length" class="bg-white pd-24 br-16 centered">
+                <div>There are no parents</div>
+            </div>
+            <div v-else-if="computedItem.length" class="grid table-body">
                 <div class="grid-item table-head grid-col-parents">
                     <h4 class="table-cell wrap-text wrap-line-1">Name</h4>
                     <h4 class="table-cell wrap-text wrap-line-1">Kids</h4>
                     <h4 class="table-cell wrap-text wrap-line-1">Role</h4>
                     <h4 class="table-cell wrap-text wrap-line-1">Contact</h4>
                 </div>
-                <div v-if="!parents.length" class="bg-white pd-24 br-16 centered">
-                    <div>There are no parents found</div>
-                </div>
-                <parent-row v-for="parent in parents" :key="parent.id" :parent="parent" />
+                <parent-row v-for="parent in computedItem" :key="parent.id" :parent="parent" />
             </div>
+            <div v-else><strong>No search result for:</strong> <i>{{ search }}</i></div>
         </div>
     </section>
 </template>
@@ -34,14 +35,26 @@
 import ParentRow from '@/components/includes/app/ParentRow.vue'
 import { mapState } from 'vuex'
 export default {
-  components: { ParentRow },
+    components: { ParentRow },
     name: 'ParentsPage',
     computed: {
         ...mapState({
             parents: (state) => state.parents
-        })
+        }),
+        computedItem() {
+            if(this.search !=='')
+            return this.parents.filter(item => {
+                return item.name.toLowerCase().match(this.search.toLowerCase()) || item.relationship.toLowerCase().match(this.search.toLowerCase())
+            })
+            else
+            return this.parents
+        }
+    },
+    data() {
+        return {
+            search: '',
+        }
     }
-
     
 }
 </script>
