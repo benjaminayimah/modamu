c5f91a09e4eecb51832c12205605ba1e99ea733d
Benjamin <benjaminayimah@gmail.com>
2023-07-04 03:40:53 +0000

    updates

--- src/views/app/parents/EventsNear.vue
+++ src/views/app/parents/EventsNear.vue
@@ -1,14 +1,11 @@
 <template>
     <section class="section-main">
-        <div class="title-row flx gap-16 ai-c">
-            <!-- <div class="flx gap-8 column">
-                <h1 class="title">Events close to you</h1>
-            </div> -->
-            <div class="secondary-input-wrapper input-wrapper" id="search_radius_wrapper">
-                <label for="searchRadiusInput">Search radius(km)</label>
-                <input id="searchRadiusInput" v-model="form.radius" autocomplete="off" data-state="location" @input="isFocusedIn('search_radius_wrapper')" @focusin="isFocusedIn('search_radius_wrapper')" @focusout="isFocusedOut('search_radius_wrapper', 'searchRadiusInput')" type="number" placeholder="" class="form-control">
-            </div>
+        <div class="title-row flx gap-24 ai-c">
             <div class="flx gap-8">
+                <div class="secondary-input-wrapper input-wrapper" id="search_radius_wrapper">
+                    <label for="searchRadiusInput">Search radius(km)</label>
+                    <input id="searchRadiusInput" v-model="form.radius" autocomplete="off" data-state="location" @input="isFocusedIn('search_radius_wrapper')" @focusin="isFocusedIn('search_radius_wrapper')" @focusout="isFocusedOut('search_radius_wrapper', 'searchRadiusInput')" type="number" placeholder="" class="form-control">
+                </div>
                 <div class="secondary-input-wrapper input-wrapper" :class="{'show-err' : noInput}" id="location_search_wrapper">
                     <label for="locationSearchInput">Enter your preferred location</label>
                     <i>
@@ -18,7 +15,7 @@
                     </i>
                     <input id="locationSearchInput" v-model="inputVal" ref="address" autocomplete="off" data-state="location" data-type="icon" @input="isFocusedIn('location_search_wrapper')" @focusin="isFocusedIn('location_search_wrapper')" @focusout="isFocusedOut('location_search_wrapper', 'locationSearchInput')" type="search" name="location" placeholder="" class="form-control">
                 </div>
-                <button @click="doManualSearch(form.latitude, form.longitude)" class="button-primary btn-lg gap-8">
+                <button @click="doManualSearch(form.latitude, form.longitude)" class="button-primary btn-lg gap-8 location-submit">
                     <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 13.974 13.974">
                         <path d="M-3384.438-704.614l-1.494-1.5c-1.017.746-2.525,1.083-4.788,1.083-2.569,0-4.164-.435-5.174-1.408-1.052-1.015-1.521-2.646-1.521-5.288s.469-4.273,1.521-5.288c1.01-.974,2.605-1.408,5.174-1.408s4.164.434,5.174,1.408c1.053,1.014,1.522,2.645,1.522,5.288,0,2.244-.338,3.759-1.085,4.786l1.5,1.5a.583.583,0,0,1,0,.824.58.58,0,0,1-.412.171A.582.582,0,0,1-3384.438-704.614Zm-10.647-11.556c-.795.766-1.165,2.18-1.165,4.448s.37,3.682,1.165,4.449c.775.747,2.121,1.08,4.364,1.08s3.589-.333,4.364-1.08c.795-.767,1.165-2.181,1.165-4.449s-.37-3.682-1.165-4.448c-.775-.748-2.12-1.081-4.364-1.081S-3394.311-716.918-3395.085-716.17Z" transform="translate(3397.417 718.417)" fill="#fff"/>
                     </svg>
@@ -262,5 +259,9 @@ section {
     label {
         color: var(--error) !important;
     }
+}
+.location-submit {
+    padding-left: 25px;
+    padding-right: 25px;
 }
 </style>
