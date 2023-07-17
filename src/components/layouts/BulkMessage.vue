9c42ea6a523397c94c3de55a68ff2650445fadde
Benjamin <benjaminayimah@gmail.com>
2023-07-17 01:06:44 +0000

    update

--- src/components/layouts/BulkMessage.vue
+++ src/components/layouts/BulkMessage.vue
@@ -22,9 +22,9 @@
                         <div v-else class="stack-cards ai-c">
                             <profile-avatar v-for="item in selections.slice(0, 6)" :key="item.id" :id="item.id" :image="item.image" />
                             <div v-if="selections.length > 6" class="fw-700 bg-img counter bg-white centered br-32">+{{ selections.length - 6 }} Recipients</div>
-                            <button @click="selectContact" class="br-50 edit-btn button-alt">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 16.919 16.166">
-                                    <path d="M-3593.823-882.342a.749.749,0,0,1-.2-.713l.857-3.427a.75.75,0,0,1,.2-.348l10.708-10.708a2.555,2.555,0,0,1,1.816-.751,2.55,2.55,0,0,1,1.815.751,2.57,2.57,0,0,1,0,3.631l-10.708,10.708a.749.749,0,0,1-.348.2l-3.427.857a.753.753,0,0,1-.181.022A.751.751,0,0,1-3593.823-882.342Zm12.624-14.134-10.561,10.561-.5,2.012,2.012-.5,10.561-10.561a1.067,1.067,0,0,0,0-1.509,1.059,1.059,0,0,0-.754-.312A1.063,1.063,0,0,0-3581.2-896.476Zm-4.385,14.353a.75.75,0,0,1-.75-.75.75.75,0,0,1,.75-.75h7.709a.75.75,0,0,1,.75.75.75.75,0,0,1-.75.75Z" transform="translate(3594.043 898.288)" fill="#0173ff"/>
+                            <button @click="selectContact" title="Add recipients" class="br-50 edit-btn bg-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 15.132 15.132">
+                                    <path d="M-1983.684,13.883V8.816h-5.066a1.25,1.25,0,0,1-1.25-1.25,1.25,1.25,0,0,1,1.25-1.25h5.066V1.25a1.25,1.25,0,0,1,1.25-1.25,1.25,1.25,0,0,1,1.249,1.25V6.316h5.066a1.25,1.25,0,0,1,1.25,1.25,1.25,1.25,0,0,1-1.25,1.25h-5.066v5.066a1.249,1.249,0,0,1-1.249,1.249A1.249,1.249,0,0,1-1983.684,13.883Z" transform="translate(1990)" fill="#0173ff"/>
                                 </svg>
                             </button>
                         </div>
@@ -179,6 +179,7 @@ label {
         height: 50px;
         width: 50px;
         margin-left: 20px;
+        box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.1);
     }
 }
 
