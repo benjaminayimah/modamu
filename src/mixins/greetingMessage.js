fed48b91e2dc904d73a759ad58d5e793b385d956
Benjamin <benjaminayimah@gmail.com>
2023-08-20 02:13:48 +0530

    production

---
+++ src/mixins/greetingMessage.js
@@ -0,0 +1,15 @@
+export default {
+    methods: {
+        getGreetingMessage() {
+            const currentTime = new Date();
+            const currentHour = currentTime.getHours()
+            if (currentHour >= 5 && currentHour < 12) {
+              return "Good morning";
+            } else if (currentHour >= 12 && currentHour < 18) {
+              return "Good day";
+            } else {
+              return "Good evening";
+            }
+        }
+    },
+}
