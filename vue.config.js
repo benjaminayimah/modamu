7b28d10d0092c9c4c980f9cd032fa5d26b3512a3
Benjamin <benjaminayimah@gmail.com>
2023-08-25 01:11:52 +0530

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-      proxy: 'https://api.modamuvillage.com'
-  },
+  // devServer: {
+  //     proxy: 'http://127.0.0.1:8000'
+  // },
   css: {
     loaderOptions: {
       sass: {
